c2356e3e5a408c7829c99013863e21f7291a5434
Sergey Alexandrov <alexandrov88@gmail.com>
2024-01-17 22:41:16 +0100

    Add init and str

--- llm_ollama.py
+++ llm_ollama.py
@@ -10,5 +10,14 @@ class Ollama(llm.Model):
     model_id = "ollama"
     can_stream: bool = False  # TODO: Implement streaming support
 
+    def __init__(
+        self,
+        model_id: str,
+    ) -> None:
+        self.model_id = model_id
+
+    def __str__(self) -> str:
+        return f"Ollama: {self.model_id}"
+
     def execute(self, prompt, stream, response, conversation):
         return ["hello world"]
