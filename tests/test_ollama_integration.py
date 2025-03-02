f5adb9adb37c08425db1cb580198e86d1867a53f
Adam Compton <comptona@gmail.com>
2025-03-01 16:27:36 -0800

    Adding support for schemas

--- tests/test_ollama_integration.py
+++ tests/test_ollama_integration.py
@@ -1,6 +1,8 @@
+import json
+
 import pytest
 
-from llm import get_async_model
+from llm import get_async_model, schema_dsl
 
 
 @pytest.mark.integration
@@ -11,3 +13,21 @@ async def test_async_model_prompt():
     response = model.prompt("a short poem about tea")
     response_text = await response.text()
     assert len(response_text) > 0
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio
+async def test_async_model_prompt_with_schema():
+    """Tests actual run. Needs llama3.2"""
+    model = get_async_model("llama3.2:latest")
+    response = model.prompt(
+        "Describe a nice dog with a surprising name",
+        schema=schema_dsl("name, age int, bio"),
+    )
+    response_text = await response.text()
+    assert len(response_text) > 0
+    json_response = json.loads(response_text)
+    assert "name" in json_response
+    assert "bio" in json_response
+    assert "age" in json_response
+    assert isinstance(json_response["age"], int)
